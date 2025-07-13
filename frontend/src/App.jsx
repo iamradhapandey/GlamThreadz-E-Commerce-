09df3265b9ff1cb0dbcdf9a558760985813cc871
radhapandeyssm37@gmail.com <radhapandeyssm37@gmail.com>
2025-07-13 16:08:36 +0530

    created navbar for my project

---
+++ frontend/src/App.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+
+import { About } from './pages/About';
+import { Collection } from './pages/Collection';
+import { Cart } from './pages/Cart';
+import { Contact } from './pages/Contact';
+import { Login } from './pages/Login';
+import { Product } from './pages/Product';
+import { Home } from './pages/Home';
+import { Placeorder } from './pages/Placeorder';
+import { Order } from './pages/Orders';
+import { Navbar } from './components/Navbar';
+
+export const App = () => {
+  return (
+    <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/collection' element={<Collection />} />
+        <Route path='/cart' element={<Cart />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/orders' element={<Order />} />
+        <Route path='/place-order' element={<Placeorder />} />
+        <Route path='/product/:productId' element={<Product />} />
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
