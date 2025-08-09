6c9421bc7a5c8db23cfbffd6ecaaa7fc698cfd80
Tulshipandey <tulshiprashadpandey892@gmail.com>
2025-08-09 14:49:37 +0530

    implement user authentication with registration and login routes; configure MongoDB and Cloudinary connections; set up environment variables; add product and user models; update package.json with necessary dependencies

---
+++ backend/controllers/userController.js
@@ -0,0 +1,68 @@
+import userModel from "../models/userModel.js";
+import validator from "validator";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+
+const createToken = (id) => {
+    return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: '3d' });
+
+}
+
+
+// Route for user login
+const loginUser = async (req, res) => {
+
+}
+
+// Route for user registration
+const registerUser = async (req, res) => {
+    try {
+        const {name , email, password} = req.body;
+        // checking user already exists or not
+        const exist = await userModel.findOne({email});
+        if(exist) {
+            return res.json({success : false , msg: "User already exists"});
+        }
+
+        // validating email format and strong password
+        if(!validator.isEmail(email)) {
+            return res.json({success : false , msg: "Invalid email format"});
+        }
+        if(password.length < 8) {
+            return res.json({success : false , msg: "please enter a strong password"});
+        } 
+
+        // hashing password
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        const newUser = new userModel({
+            name,
+            email,
+            password: hashedPassword
+        });
+
+        const user = await newUser.save();
+
+        const token = createToken(user._id);
+        res.json({
+            success: true,
+            token})
+
+        
+    } catch (error) {
+        console.log(error)
+        res.json({ success: false, msg: error.message });
+        
+    }
+
+}
+
+// Route for admin login
+const adminLogin = async (req, res) => {
+
+}
+
+
+export { loginUser, registerUser , adminLogin };
--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,29 @@
 import express from 'express';
-import mongoose from 'mongoose';
 import cors from 'cors';
-import dotenv from 'dotenv';
+import 'dotenv/config';
+import connectDB from './config/mongodb.js';
+import connectCloudinary from './config/cloudinary.js';
+import userRouter from './routes/userRoute.js';
 
+// APP Config 
 const app = express();
+const PORT = process.env.PORT || 4000;
+connectDB()
+connectCloudinary();
 
-app.use(cors());
+// middlewares 
 app.use(express.json());
+app.use(cors());
 
-dotenv.config();
-const PORT = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
-  res.send('Welcome to GlamThreadz E-Commerce Backend!');
-});
+// api endpoints 
+app.use('/api/user' , userRouter)
+
+
+app.get('/', (req, res)=>{
+    res.send('Welcome to the API');
+})
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
 });
