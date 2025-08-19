8d90b5fcd18986824483ec1b5cbdcbe6cef5f701
Tulshipandey <tulshiprashadpandey892@gmail.com>
2025-08-19 23:21:33 +0530

    feat: implement admin authentication middleware and routes for admin login; secure product management routes

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,11 +1,12 @@
 import express from "express" ;
 import {listProduct, addProduct, removeProduct, singleProduct} from "../controllers/productController.js";
 import upload from "../middleware/multer.js"; // Assuming you have a middleware for handling file uploads
+import adminAuth from "../middleware/adminAuth.js"; // Assuming you have an admin authentication middleware
 
 const productRouter = express.Router() ;
-productRouter.post('/add',upload.fields([{name: 'image1', maxCount:1},{name: 'image2', maxCount:1},{name: 'image3', maxCount:1},{name: 'image4', maxCount:1}]), addProduct);
+productRouter.post('/add',adminAuth,upload.fields([{name: 'image1', maxCount:1},{name: 'image2', maxCount:1},{name: 'image3', maxCount:1},{name: 'image4', maxCount:1}]), addProduct);
 productRouter.get('/list', listProduct);
-productRouter.post('/remove', removeProduct);
+productRouter.post('/remove',adminAuth, removeProduct);
 productRouter.get('/single', singleProduct)
 
 export default productRouter;
